ee1c24c5d337adde54472de223a271323c1c99a4
Sahil Kotian <sahil.kotian316@gmail.com>
2026-04-01 08:44:44 -0500

    chore: add debug api request logging for mobile

--- apps/mobile/lib/features/charts/data/chart_api_client.dart
+++ apps/mobile/lib/features/charts/data/chart_api_client.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 import '../../../config/app_config.dart';
@@ -29,11 +30,13 @@ class ChartApiClient {
 
   Future<ComputeChartResponse> computeChart(ComputeChartRequest request) async {
     final uri = Uri.parse('$_baseUrl/v1/charts/compute');
+    _logRequest('POST', uri);
     final response = await _httpClient.post(
       uri,
       headers: const <String, String>{'Content-Type': 'application/json'},
       body: jsonEncode(request.toJson()),
     );
+    _logResponse('POST', uri, response.statusCode);
     final body = _decode(response.body);
     _throwIfError(response.statusCode, body);
     return ComputeChartResponse.fromJson(body);
@@ -43,11 +46,13 @@ class ChartApiClient {
     ComputeChartRequest request,
   ) async {
     final uri = Uri.parse('$_baseUrl/v1/reports/compute');
+    _logRequest('POST', uri);
     final response = await _httpClient.post(
       uri,
       headers: const <String, String>{'Content-Type': 'application/json'},
       body: jsonEncode(request.toJson()),
     );
+    _logResponse('POST', uri, response.statusCode);
     final body = _decode(response.body);
     _throwIfError(response.statusCode, body);
     return ComputeReportResponse.fromJson(body);
@@ -57,7 +62,9 @@ class ChartApiClient {
     final uri = Uri.parse(
       '$_baseUrl/v1/places/search',
     ).replace(queryParameters: <String, String>{'query': query});
+    _logRequest('GET', uri);
     final response = await _httpClient.get(uri);
+    _logResponse('GET', uri, response.statusCode);
     final body = _decode(response.body);
     _throwIfError(response.statusCode, body);
     return PlaceSearchResponse.fromJson(body);
@@ -86,5 +93,19 @@ class ChartApiClient {
       (body['detail'] as String?) ?? 'Request failed',
       statusCode: statusCode,
     );
+  }
+
+  void _logRequest(String method, Uri uri) {
+    if (!kDebugMode) {
+      return;
+    }
+    debugPrint('[TRIKAAL_API] -> $method $uri');
+  }
+
+  void _logResponse(String method, Uri uri, int statusCode) {
+    if (!kDebugMode) {
+      return;
+    }
+    debugPrint('[TRIKAAL_API] <- $statusCode $method $uri');
   }
 }
